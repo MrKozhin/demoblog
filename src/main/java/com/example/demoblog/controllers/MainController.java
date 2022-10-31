e35fb5103cdec982a7d3234bb923307696124a35
Evgenii Kozhin <kozhin.post@gmail.com>
2022-10-31 09:19:11 +0300

    Templates added Controller added

--- src/main/java/com/example/demoblog/controllers/MainController.java
+++ src/main/java/com/example/demoblog/controllers/MainController.java
@@ -13,5 +13,10 @@ public class MainController {
         model.addAttribute("title", "Главная страница");
         return "home";
     }
+    @GetMapping("/about")
+        public String about(Model model) {
+            model.addAttribute("title", "О нас");
+            return "about";
+        }
 
 }
